dd6b94f619110fab4caffd46faf94149917a7b5f
Alibek Erkabayev <alibek060395@gmail.com>
2025-11-19 19:31:56 +0300

    feat: main methods for queue

---
+++ Queue in C++ STL/run.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+# Run script for C++ Project Template
+./build/CppProjectTemplate
---
+++ Queue in C++ STL/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "queue.hpp"
+
+int main(int argc, char *argv[])
+{
+    run_queue();
+    std::cout << "***************************************" << std::endl;
+    run_insert2queue();
+    std::cout << "***************************************" << std::endl;
+    run_access2queue();
+    std::cout << "***************************************" << std::endl;
+    delete_from_queue();
+    std::cout << "***************************************" << std::endl;
+    check_empty_queue();
+    std::cout << "***************************************" << std::endl;
+    size_of_queue();
+    std::cout << "***************************************" << std::endl;
+
+    return 0;
+}
